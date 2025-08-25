bc625d4c043653fa90a25ddb55515982c9d45ed0
Hamid Rana <ranahamid728@gmail.com>
2025-08-25 23:52:57 +0500

    variables types of javascipt

---
+++ jsWithBabbar/index.js
@@ -0,0 +1,12 @@
+// var a = 30;
+// var a = 32;
+// a = 56;
+
+// let a = 23;
+// let a = 45; // redecleration is not allowed
+// a = 23
+
+// const a = 234;
+// // const a = 33;  // redecleration is not 
+// a = 43;   // reassignment is not allowed
+console.log(a);
