c41a01be7e2b9a34c49bef2268d85812216d3103
Hamid Rana <ranahamid728@gmail.com>
2025-08-26 22:31:38 +0500

    ternary and conditional operator with truthy falsey and short circutting concepts

--- jsWithBabbar/index.js
+++ jsWithBabbar/index.js
@@ -9,6 +9,7 @@
 // const a = 234;
 // // const a = 33;  // redecleration is not 
 // a = 43;   // reassignment is not allowed
+
 // Arithmetic operator
 let a = 5;
 let b = 3
@@ -18,6 +19,7 @@ let b = 3
 // console.log(a/b);
 // console.log(a%b);
 // console.log(a**b);
+
 // assignment operator
 // a = a+5;
 // a +=5;
@@ -25,6 +27,7 @@ let b = 3
 // a -=5;
 // a *= 5
 // a /= 5;
+
 // Comparison Opreator 
 // console.log(10<5);
 // console.log(10>5);
@@ -38,3 +41,22 @@ let b = 3
 // console.log(5!==5);
 // console.log(5!=5);
 // console.log('10'!=5);
+
+// Ternary Operator
+// let age = 3;
+// let voterStatus = (age > 18)? "i can Vote": "I cannot Vote";
+// console.log(voterStatus)
+
+// conditional operator
+
+// let ans = (true && true && true);
+// let ans = (true && false && true);
+// let ans = (true || false || false);
+// let ans = (false || false || false );
+// let ans = (!true);
+// let ans = (!false);
+// console.log(false && 'hamid') // it wil return false
+// console.log(true && 'hamid') // this is true because this is truthy
+// console.log(false && 8) // this is false 
+// console.log(false || 'hamid') // this is truthy due to OR result will be 
+console.log(false || 8 || 21)
