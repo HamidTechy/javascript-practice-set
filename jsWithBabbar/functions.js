c0bbe8348760423c9050f91c64211dddbdcb5b96
Hamid Rana <ranahamid728@gmail.com>
2025-08-28 23:54:32 +0500

    completed practice of functions with arrow functions and expression and others way

--- jsWithBabbar/functions.js
+++ jsWithBabbar/functions.js
@@ -35,9 +35,64 @@
 // let takeSum  = returnSum(3, 4, 5)
 // console.log(takeSum)
 
-function getName(fName, lName){
-    let fullName = fName + lName;
-    return fullName;
+//string as argument in functions 
+// function getName(fName, lName){
+//     let fullName = fName + " "+ lName;
+//     return fullName;
+// }
+// let userName = getName("Hamid", "Latif");
+// console.log(userName)
+
+// without space in concatination it will be concidered by as int type 
+// function getName(fName, lName){
+//     let fullName = fName + lName;
+//     return fullName;
+// }
+// let userName = getName(23, 12);
+// console.log(userName)
+
+// function expression
+
+// let getMultiply = function(a, b){
+//     return a * b;
+// }
+// let ans = getMultiply(2,5)
+// console.log(ans)
+
+// let getsquare = function(a){
+//     return a**2;
+// }
+// let answer = getsquare(5)
+// console.log(answer)
+
+// Arrow function
+// let getMultiply = (a, b) =>{
+//     return a*b;
+// }
+// let ans = getMultiply(3, 5)
+// console.log(ans)
+
+
+// All three ways to write function
+//normal function
+function getSum(a, b){
+    return a + b;
 }
-let userName = getName("Hamid", " Latif");
-console.log(userName)
+let ans1 = getSum(3,4)
+console.log(ans1)
+
+// function expression
+
+const getMultiply = function (a, b){
+    return a * b;
+}
+let ans2 = getMultiply(3,4)
+console.log(ans2)
+
+// Arrow function
+
+const getdivision = (a, b)=>{
+    return a / b;
+}
+let ans3 = getdivision(12,4)
+console.log(ans3)
