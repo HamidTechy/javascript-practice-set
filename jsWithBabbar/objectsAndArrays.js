2754fef47948f0c6ae002475d6f988c0f5803da1
Hamid Rana <ranahamid728@gmail.com>
2025-08-29 17:37:35 +0500

    array in javascript

--- jsWithBabbar/objectsAndArrays.js
+++ jsWithBabbar/objectsAndArrays.js
@@ -1,13 +1,26 @@
-let obj = {
-    "user Name": "Hamid Rana",
-    age : 27,
-    qualifcation : "BSit",
-    greet : ()=>{
-        console.log("Assalam o Alaikum")
-    }
-}
+// let obj = {
+//     "user Name": "Hamid Rana",
+//     age : 27,
+//     qualifcation : "BSit",
+//     greet : ()=>{
+//         console.log("Assalam o Alaikum")
+//     }
+// }
 
-// console.log(obj["user Name"])
-// console.log(obj.age)
-obj.greet()
-console.log(obj.greet())
+// // console.log(obj["user Name"])
+// // console.log(obj.age)
+// // obj.greet()
+// let obj2 = obj
+// console.log(obj2)
+// // console.log(obj.greet())
+// console.log(obj)
+
+// Array creation
+
+let brr = [32, 4, 65, 75, 23];
+console.log(typeof(brr));
+// array constructor 
+
+const arr = new Array("Hamid", 85, true, 70.5);
+console.log(arr);
+console.log(arr[2]) // how to check value using index
