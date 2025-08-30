bdc962b7ba83dcddc1fc99ed329444e319b65b7c
Hamid Rana <ranahamid728@gmail.com>
2025-08-30 22:44:01 +0500

    slice and Splice methods of arry

--- jsWithBabbar/objectsAndArrays.js
+++ jsWithBabbar/objectsAndArrays.js
@@ -17,18 +17,26 @@
 
 // Array creation
 
-let brr = [32, 4, 65, 75, 23];
-console.log(typeof(brr));
-// array constructor 
+// let brr = [32, 4, 65, 75, 23];
+// console.log(typeof(brr));
+// // array constructor 
 
-const arr = new Array("Hamid", 85, true, 70.5);
-console.log(arr);
-console.log(arr[2]) // how to check value using index
+// const arr = new Array("Hamid", 85, true, 70.5);
+// console.log(arr);
+// console.log(arr[2]) // how to check value using index
 
 let myArr = [23, 34, 65, 76]
 // myArr.push(34)
 // myArr.pop()
 // myArr.shift()
-myArr.unshift(45)
 
+// myArr.unshift(45)
+// myArr.push(12)
+// myArr.push(42)
+// myArr.push(92)
+
+// console.log(myArr.slice(2,5))
+
+// myArr.splice(2, 1, "Hamid")
 console.log(myArr)
+
