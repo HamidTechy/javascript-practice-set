e9d5a5cb2ee60085d0d112b6282f98d3450bba47
Hamid Rana <ranahamid728@gmail.com>
2025-09-01 15:47:02 +0500

    anonymous function or inner function

--- jsWithBabbar/functionsHoisting.js
+++ jsWithBabbar/functionsHoisting.js
@@ -41,3 +41,47 @@
 // }
 
 // greetMe(greet, "Hamid")
+
+// return function
+
+// function solve(number){
+//     return function(number){
+//         return number * number
+//     }
+// }
+// let ans = solve(5)
+
+// console.log(ans(10))
+
+// another way to return function
+
+// function solve(){
+//     return function (number){
+//         return number * number
+//     }
+// }
+
+// let ans = solve()
+// console.log(ans(5))
+
+// return function with string 
+
+// function greet(salam){
+//     return function(name){
+//         return `${salam} ${name}`
+//     }
+// }
+
+// let ans = greet("assalam o alaikum")
+// console.log(ans("Hamid"))
+
+// variable with string in inner function
+
+// function person(name){
+//     return function(year){
+//         return `${name} is ${year*12} months old`
+//     }
+// }
+// let ans = person("Hamid")
+
+// console.log(ans(27))
