929f822b4e7b7095755ab555c22755d292e347a3
Hamid Rana <ranahamid728@gmail.com>
2025-09-02 14:35:15 +0500

    function expression and function to another function

--- jsWithBabbar/functionsHoisting.js
+++ jsWithBabbar/functionsHoisting.js
@@ -12,6 +12,32 @@
 
 // variable hoisting
 
-console.log(age)
-var age = 26;
+// console.log(age)
+// var age = 26;
 
+// const obj1 = new Human();
+// class Human{
+
+// }
+
+
+// function expression
+// let myVar = function (){
+//     console.log("Hamid Rana");
+// }
+
+// myVar()
+
+// functions pass an argument
+
+// function greetMe(greet, fullName){
+//     console.log("Hello Mr", fullName)
+//     greet()
+// }
+
+
+// function greet(){
+//     console.log("Assalam o alaikum")
+// }
+
+// greetMe(greet, "Hamid")
