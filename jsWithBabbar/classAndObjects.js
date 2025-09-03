013e884d243e0578b173e6f76c3a4657034f46be
Hamid Rana <ranahamid728@gmail.com>
2025-09-03 22:24:07 +0500

    creation of class and object

---
+++ jsWithBabbar/classAndObjects.js
@@ -0,0 +1,18 @@
+
+class Human{
+    //properties
+    age = 27;
+    weight  = 70;
+    heigt = 5.11;
+
+    //behavious
+    walking(){
+        console.log("I am walking")
+    }
+    running(){
+        console.log("I am running ")
+    }
+}
+
+let a = new Human();
+a.running()
