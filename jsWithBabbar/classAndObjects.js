aa3aab425de4985723b0e86c779b7b399395d086
Hamid Rana <ranahamid728@gmail.com>
2025-09-03 23:27:38 +0500

    access private member inside the class using this keyword

--- jsWithBabbar/classAndObjects.js
+++ jsWithBabbar/classAndObjects.js
@@ -1,18 +1,19 @@
 
 class Human{
     //properties
+    //by default they are public members 
     age = 27;
-    weight  = 70;
+    #weight  = 70;  // private member
     heigt = 5.11;
 
     //behavious
     walking(){
-        console.log("I am walking")
+        console.log("I am walking right now my weight is ", this.#weight)
     }
     running(){
-        console.log("I am running ")
+        console.log("I am running ",)
     }
 }
 
-let a = new Human();
-a.running()
+let obj = new Human();
+obj.walking()
