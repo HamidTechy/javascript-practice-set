deb8a13d8aa4dd6f2561f3cc16fb27d8b9ac85f1
Hamid Rana <ranahamid728@gmail.com>
2025-09-04 22:46:32 +0500

    default parameter

--- jsWithBabbar/classAndObjects.js
+++ jsWithBabbar/classAndObjects.js
@@ -1,42 +1,50 @@
 
-class Human{
-    //properties
-    //by default they are public members 
-    city;
-    age = 27;
-    #weight  = 70;  // private member
-    heigt = 5.11;
+// class Human{
+//     //properties
+//     //by default they are public members 
+//     city;
+//     age = 27;
+//     #weight  = 70;  // private member
+//     heigt = 5.11;
 
-    constructor(newCity, newAge, newWeight){
-        this.city = newCity;
-        this.age = newAge;
-        this.#weight = newWeight;
-    }
-    //behavious
-    #walking(){
-        console.log("I am walking right now my weight is ", this.#weight)
-    }
-    running(){
-        console.log("I am running ",)
-    }
-    get fetchWeight(){
-        return this.#weight;
-    }
-    set modifyweight(val){
-        this.#weight = val
-    }
-    get fetchWalking(){
-        return this.#walking();
-    }
+//     constructor(newCity, newAge, newWeight){
+//         this.city = newCity;
+//         this.age = newAge;
+//         this.#weight = newWeight;
+//     }
+//     //behavious
+//     #walking(){
+//         console.log("I am walking right now my weight is ", this.#weight)
+//     }
+//     running(){
+//         console.log("I am running ",)
+//     }
+//     get fetchWeight(){
+//         return this.#weight;
+//     }
+//     set modifyweight(val){
+//         this.#weight = val
+//     }
+//     get fetchWalking(){
+//         return this.#walking();
+//     }
 
+// }
+
+// let obj = new Human("Sheikhupura", 28, 78);
+// // console.log(obj.fetchWeight)
+// // obj.walking();
+
+// // console.log(obj.city)
+// // console.log(obj.age)
+// // console.log(obj.fetchWeight);
+
+// obj.fetchWalking
+
+
+// default parameter
+function sayName(name="Latif"){
+    console.log(name)
 }
+sayName()
 
-let obj = new Human("Sheikhupura", 28, 78);
-// console.log(obj.fetchWeight)
-// obj.walking();
-
-// console.log(obj.city)
-// console.log(obj.age)
-// console.log(obj.fetchWeight);
-
-obj.fetchWalking
