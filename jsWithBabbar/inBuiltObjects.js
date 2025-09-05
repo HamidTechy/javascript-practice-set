905498fa41d92101818768722f9a0d976bb88012
Hamid Rana <ranahamid728@gmail.com>
2025-09-06 00:08:32 +0500

    different ways to write date and how to get and set date

--- jsWithBabbar/inBuiltObjects.js
+++ jsWithBabbar/inBuiltObjects.js
@@ -1,27 +1,38 @@
-console.log(Math.PI)
+// console.log(Math.PI)
 
-console.log(Math.round(1.5))
+// console.log(Math.round(1.5))
 
-console.log(Math.max(12, 35, 45, 25, 65))
+// console.log(Math.max(12, 35, 45, 25, 65))
 
-console.log(Math.min(82, 35, 45, 25, 65))
+// console.log(Math.min(82, 35, 45, 25, 65))
 
-console.log(Math.floor(1.9))
+// console.log(Math.floor(1.9))
 
-console.log(Math.ceil(1.9))
+// console.log(Math.ceil(1.9))
 
-console.log(Math.abs(-8))
+// console.log(Math.abs(-8))
 
-console.log(Math.random()*99)
+// console.log(Math.random()*99)
 
-console.log(Math.sqrt(9))
+// console.log(Math.sqrt(9))
 
-console.log(Math.pow(2, 4))
+// console.log(Math.pow(2, 4))
 
 // date
-let curr = new Date()
+// different formats of date
+// let curr = new Date()
+// console.log(curr)
 
-let DOB = new Date('March 01 1997')
-console.log(curr)
+// let DOB = new Date('March 01 1997 04:16')
+
 // console.log(new Date())
-console.log(DOB)
+// console.log(DOB)
+// console.log( Date())
+
+let otherdate = new Date(1998, 2, 1, 9)
+console.log(otherdate)
+
+console.log(otherdate.getDate())
+console.log(otherdate.getFullYear())
+console.log(otherdate.setFullYear(1997))
+console.log(otherdate)
