d377156d05f69290cb2f7e3dc42bcb27120abf72
Hamid Rana <ranahamid728@gmail.com>
2025-09-05 22:52:29 +0500

    Math functions pi round max mix floor

---
+++ jsWithBabbar/inBuiltObjects.js
@@ -0,0 +1,17 @@
+console.log(Math.PI)
+
+console.log(Math.round(1.5))
+
+console.log(Math.max(12, 35, 45, 25, 65))
+
+console.log(Math.min(82, 35, 45, 25, 65))
+
+console.log(Math.floor(1.9))
+
+console.log(Math.ceil(1.9))
+
+console.log(Math.abs(-8))
+
+console.log(Math.random()*99)
+
+console.log(Math.sqrt(9))
