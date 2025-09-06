7f33c25da61037770dbb58e1725353718de84103
Hamid Rana <ranahamid728@gmail.com>
2025-09-06 13:11:25 +0500

    cloning with iteration

--- jsWithBabbar/objectCloning.js
+++ jsWithBabbar/objectCloning.js
@@ -10,16 +10,55 @@
 
 // cloning 
 
+// let src = {
+//     age: 23,
+//     wt: 79,
+//     ht: 5.11
+// }
+
+// let dest = {...src}
+// // let dest = src
+// dest.color = "red"
+// src.age = 28
+// console.log(src)
+
+// console.log(dest)
+
+// object.assign and two object cloning same time 
+
+// let src = {
+//     age: 23,
+//     wt: 79,
+//     ht: 5.11
+// }
+// let src2 = {
+//     value: 101,
+//     city: "sheikhupura",
+// }
+
+// let dest = Object.assign({}, src, src2);
+
+// src.age = 28;
+// dest.color = "bright";
+// console.log(src);
+// console.log(dest);
+
+
+//cloning with iteration
+
+
 let src = {
-    age: 23,
-    wt: 79,
-    ht: 5.11
+    age : 28,
+    wt: 80,
+    ht: 5.9
 }
 
-let dest = {...src}
-// let dest = src
-dest.color = "red"
-src.age = 28
+let dest = {};
+for(let key in src){
+    let newKey = key;
+    let newValue = src[key];
+    dest[newKey] = newValue;
+} 
+src.ht = 6;
 console.log(src)
-
 console.log(dest)
