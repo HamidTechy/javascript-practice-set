eda4e6f8be0b18d50ca0881da793e0923c6c783e
Hamid Rana <ranahamid728@gmail.com>
2025-09-06 12:12:47 +0500

    object cloning with spread operator

--- jsWithBabbar/objectCloning.js
+++ jsWithBabbar/objectCloning.js
@@ -1,8 +1,25 @@
-let obj = {
+// object dynamic nature
+// let obj = {
+//     age: 23,
+//     wt: 79,
+//     ht: 5.11
+// }
+
+// obj.color = "White"
+//  console.log(obj)
+
+// cloning 
+
+let src = {
     age: 23,
     wt: 79,
     ht: 5.11
 }
 
-obj.color = "White"
- console.log(obj)
+let dest = {...src}
+// let dest = src
+dest.color = "red"
+src.age = 28
+console.log(src)
+
+console.log(dest)
