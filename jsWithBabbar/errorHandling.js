d0f9b1cdfe21e3ec25ef5ab54ffa045df640bcd4
Hamid Rana <ranahamid728@gmail.com>
2025-09-07 23:41:18 +0500

    throw new Error with try catch and then with if condition

--- jsWithBabbar/errorHandling.js
+++ jsWithBabbar/errorHandling.js
@@ -6,15 +6,29 @@
 // reference error  
 // console.log(i)
 
-try{
-    console.log("Try block start here")
-    console.log(x)
-    console.log("try Block ends here")
-}
-catch(err){
-    console.log("Hi I am catch block")
-    // console.log("Your error is here", err)
-}
-finally{
-    console.log("I am finally block i will always run ")
+// try{
+//     console.log("Try block start here")
+//     console.log(x)
+//     console.log("try Block ends here")
+// }
+// catch(err){
+//     console.log("Hi I am catch block")
+//     // console.log("Your error is here", err)
+// }
+// finally{
+//     console.log("I am finally block i will always run ")
+// }
+
+// custome error
+
+// try {
+//     console.log(x)
+// }
+// catch{
+//     throw new Error("Hi this is custome error and here is my message: ")
+// }
+
+let errorCode = 401;
+if(errorCode == 401){
+    throw new Error("Invalid credentials")
 }
