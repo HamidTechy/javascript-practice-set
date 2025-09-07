5c92c4b42996f5de9f51f76b9061b3abe973e8a9
Hamid Rana <ranahamid728@gmail.com>
2025-09-07 22:58:34 +0500

    try{} and catch{} for error handling

--- jsWithBabbar/errorHandling.js
+++ jsWithBabbar/errorHandling.js
@@ -6,3 +6,12 @@
 // reference error  
 // console.log(i)
 
+try{
+    console.log("Try block start here")
+    console.log(x)
+    console.log("try Block ends here")
+}
+catch(err){
+    console.log("Hi I am catch block")
+    // console.log("Your error is here", err)
+}
