402aa757ededf62f36b44f4731118c8ceb341b9e
Hamid Rana <ranahamid728@gmail.com>
2025-10-10 22:16:37 +0500

    revised date methods

--- jsWithBabbar/countDownTImer/script.js
+++ jsWithBabbar/countDownTImer/script.js
@@ -1,3 +1,3 @@
-setInterval(() => {
-    console.log("Hello Hamid Rana")
-}, 1000);
+const enddate = new Date('29 oct, 2025, 22:00:00').getDate()
+const startdate = new Date().getTime()
+console.log(startdate)
