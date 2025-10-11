d1a6e6608024f326a86b652391a96730fd0ede25
Hamid Rana <ranahamid728@gmail.com>
2025-10-11 21:23:20 +0500

    added logic of timer js code

--- jsWithBabbar/countDownTImer/script.js
+++ jsWithBabbar/countDownTImer/script.js
@@ -1,3 +1,21 @@
-const enddate = new Date('29 oct, 2025, 22:00:00').getDate()
-const startdate = new Date().getTime()
-console.log(startdate)
+const endDate = new Date('15 nov, 2025 20:00:00').getTime()
+const startDate = new Date().getTime()
+
+
+function updateTimer(){
+    const now = new Date().getTime();
+    const distanceCovered = now - startDate;
+    const distancePending = endDate - now;
+
+    const days = Math.floor(distancePending/(24*60*60*1000));
+    const hrs = Math.floor((distancePending%(24*60*60*1000))/(60*60*1000));
+    const mins = Math.floor((distancePending%(60*60*1000))/(60*1000));
+    const secs = Math.floor((distancePending%(60*1000))/(1000));
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hrs;
+    document.getElementById("minutes").innerHTML = mins;
+    document.getElementById("seconds").innerHTML = secs;
+}
+
+setInterval(updateTimer, 1000);
