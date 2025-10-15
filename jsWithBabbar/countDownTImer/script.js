6eabe8ef77fa9547ffb972e8a46be98323d9648b
Hamid Rana <ranahamid728@gmail.com>
2025-10-15 21:52:27 +0500

    improve method of setinterval method

--- jsWithBabbar/countDownTImer/script.js
+++ jsWithBabbar/countDownTImer/script.js
@@ -1,20 +1,21 @@
-const endDate = new Date('1 oct, 2025 20:00:00').getTime()
-const startDate = new Date("30 oct, 2025 20:00:00").getTime()
+const endDate = new Date("1 oct, 2025 20:00:00").getTime();
+const startDate = new Date("30 oct, 2025 20:00:00").getTime();
 
-
-function updateTimer(){
+let timer = setInterval(function updateTimer() {
     const now = new Date().getTime();
     const distanceCovered = now - startDate;
     const distancePending = endDate - now;
 
-    const oneDayInMillis = (24*60*60*1000);
-    const oneHourInMillis = (60*60*1000)
-    const oneMinInMillis = (60*1000)
-    const oneSecondInMillis = (1000)
-    const days = Math.floor(distancePending/oneDayInMillis);
-    const hrs = Math.floor((distancePending%(oneDayInMillis))/oneHourInMillis);
-    const mins = Math.floor((distancePending%(oneHourInMillis))/oneMinInMillis);
-    const secs = Math.floor((distancePending%(oneMinInMillis))/oneSecondInMillis)    ;
+    const oneDayInMillis = 24 * 60 * 60 * 1000;
+    const oneHourInMillis = 60 * 60 * 1000;
+    const oneMinInMillis = 60 * 1000;
+    const oneSecondInMillis = 1000;
+    const days = Math.floor(distancePending / oneDayInMillis);
+    const hrs = Math.floor((distancePending % oneDayInMillis) / oneHourInMillis);
+    const mins = Math.floor((distancePending % oneHourInMillis) / oneMinInMillis);
+    const secs = Math.floor(
+        (distancePending % oneMinInMillis) / oneSecondInMillis
+    );
 
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hrs;
@@ -23,15 +24,13 @@ function updateTimer(){
 
     // calculate width percentage for progress bar
     const totalDistance = endDate - startDate;
-    const percentageDistance = (distanceCovered/totalDistance)*100;
-    document.getElementById("progress-bar").style.width = percentageDistance + "%";
+    const percentageDistance = (distanceCovered / totalDistance) * 100;
+    document.getElementById("progress-bar").style.width =
+        percentageDistance + "%";
 
-    if(distancePending < 0){
-        clearInterval()
+    if (distancePending < 0) {
+        clearInterval(timer);
         document.getElementById("countdown").innerHTML = "Experired";
-        document.getElementById("progress-bar").style.width = "100%"
+        document.getElementById("progress-bar").style.width = "100%";
     }
-
-}
-
-setInterval(updateTimer, 1000);
+}, 1000);
