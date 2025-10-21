e4d02b7a127eb2bb2add2a10b682ab2b6f4a8691
Hamid Rana <ranahamid728@gmail.com>
2025-10-21 20:35:45 +0500

    added statscontainer and easyprogresscircle

--- jsWithBabbar/leetCodeMatricsApp/script.js
+++ jsWithBabbar/leetCodeMatricsApp/script.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const searchButton = document.getElementById("search-btn");
     const usernameInput = document.getElementById("user-input");
+    const statsContainer = document.querySelector(".stats-container");
+    const easyProgressCircle = document.querySelector(".easy-progress")
 
 })
