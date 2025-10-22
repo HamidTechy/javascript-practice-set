3152fb4a51daf42a0b26477146d4ce3cb54135ef
Hamid Rana <ranahamid728@gmail.com>
2025-10-22 20:32:32 +0500

    get element of searcbutton and userinput

--- jsWithBabbar/leetCodeMatricsApp/script.js
+++ jsWithBabbar/leetCodeMatricsApp/script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+    const searchButton = document.getElementById("search-btn");
+    const usernameInput = document.getElementById("user-input");
+
 })
