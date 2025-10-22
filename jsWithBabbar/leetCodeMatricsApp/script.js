7ae5ff984f9055c0e4fb420c8451b552575fab3e
Hamid Rana <ranahamid728@gmail.com>
2025-10-22 20:37:46 +0500

    added medium and hard progress circle in js file

--- jsWithBabbar/leetCodeMatricsApp/script.js
+++ jsWithBabbar/leetCodeMatricsApp/script.js
@@ -4,5 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const usernameInput = document.getElementById("user-input");
     const statsContainer = document.querySelector(".stats-container");
     const easyProgressCircle = document.querySelector(".easy-progress")
+    const mediumProgressCircle = document.querySelector(".medium-progress")
+    const hardProgressCircle = document.querySelector(".hard-progress")
 
 })
